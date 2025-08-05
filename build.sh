1ac8d10fbdb3d0e0c30cd1cba58e565c39c66d60
Da Xue <da@libre.computer>
2025-08-05 11:21:23 -0400

    gxl: align with upstream and add HDMI only for AML-S905X-CC-V2

--- build.sh
+++ build.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+package=libretech-alsa-ucm-conf
 dateymd=$(date +%Y.%m.%d)
 commit=$(git rev-parse HEAD)
 dateutc=$(date -Ru)
 cat <<EOF > debian/changelog
-libretech-alsa-ucm-conf ($dateymd) linux; urgency=medium
+$package ($dateymd) linux; urgency=medium
 
   * $commit
 
  -- Da Xue <da@libre.computer>  $dateutc
 EOF
 dpkg-buildpackage -uc --no-sign --build=all
+mv ../${package}_* .
